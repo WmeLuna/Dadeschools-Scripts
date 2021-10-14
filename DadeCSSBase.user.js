8330d784c4af085226da5e164e26da8f1f325be4
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2021-10-14 18:38:17 -0400

    Create DadeCSSBase.user.js

---
+++ DadeCSSBase.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         DadeCSSBase
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://mdcpsportal.dadeschools.net/*
+// @downloadURL  https://github.com/WmeLuna/Dadeschools-Scripts/raw/main/DadeCSSBase.user.js
+// @icon         https://www.google.com/s2/favicons?domain=dadeschools.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelector("body").setAttribute("WmeReady", "")
+
+    // Currently this does nothing however i wanna start having this ready to start a bigger project on the dadeschools portal
+})();
