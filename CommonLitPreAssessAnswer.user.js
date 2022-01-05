b31a9e2abfcce2006dae158d616f1d6976d85a1d
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-01-04 19:16:48 -0500

    Update CommonLitPreAssessAnswer.user.js

--- CommonLitPreAssessAnswer.user.js
+++ CommonLitPreAssessAnswer.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       WmeLuna
 // @match        https://www.commonlit.org/en/students/student_lessons/*
 // @icon         https://www.google.com/s2/favicons?domain=commonlit.org
 // @grant        unsafeWindow
