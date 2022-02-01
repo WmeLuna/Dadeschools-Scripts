3304409b9afe0de334d3e5050b0cdafaf672a055
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-02-01 12:18:10 -0500

    Add files via upload

---
+++ AutoUnblocker.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         AutoUnblocker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://localhost:6543/block_screen.html?id=*
+// @icon         https://avatars.githubusercontent.com/u/46545277
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var scr = document.createElement('script');
+    scr.type="text/javascript";
+    scr.innerText=`function b(){0==a.href.includes("localhost")?location.href="https://womginx.wmeluna.repl.co/main/"+a.href:setTimeout(b,250)}b();`
+    document.getElementsByTagName('head')[0].appendChild(scr)
+})();
