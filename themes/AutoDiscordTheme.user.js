ae152da6cab3e5f327b2aa0ca2dffffccbced64a
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-02-25 10:11:33 -0500

    Update AutoDiscordTheme.user.js

--- themes/AutoDiscordTheme.user.js
+++ themes/AutoDiscordTheme.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         AutoDiscordTheme
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       WmeLuna
 // @match        https://unlock.wmeluna.com/main/*discord.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
 // @grant        none
-// @run-at       document-start
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
