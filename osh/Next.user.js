78ef3f29c27e471dfe54e087449f8b799f3a1e1f
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-03-08 13:25:13 -0500

    Update Next.user.js

--- osh/Next.user.js
+++ osh/Next.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://campus.careersafeonline.com/view_content.k2?pageid=*
+// @match        https://campus.careersafeonline.com/view_content.k2?*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=careersafeonline.com
 // @grant        none
 // @run-at       document-end
