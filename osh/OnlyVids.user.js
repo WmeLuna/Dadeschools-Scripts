4d235573d791347c5785802aabe17c1d4b9f44f4
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-03-09 08:38:20 -0500

    Create OnlyVids.user.js

---
+++ osh/OnlyVids.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         OnlyVids
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://campus.careersafeonline.com/view_content.k2?*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=careersafeonline.com
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function next() {
+        try {
+            if (document.querySelector('#contentIframe').contentWindow.document.body.classList[0] != 'video-body') {
+                document.querySelector("#footer_next_page").click()
+            }
+            setTimeout(next, 250);
+        }
+        catch {
+            setTimeout(next, 250);
+        }
+    }
+    next()
+
+    // Your code here...
+})();
