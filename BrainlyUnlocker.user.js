cf7904d3647e6639bb2065f9286db97c1e727dd3
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-03-10 19:06:44 -0500

    Create BrainlyUnlocker.user.js

---
+++ BrainlyUnlocker.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         BrainlyUnlocker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *brainly.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=brainly.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+localStorage.setItem("flexible-funnel-previews", "");
+
+    // Your code here...
+})();
