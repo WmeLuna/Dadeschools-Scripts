f12b18526d3c74f85e53841e4de571d14ee1e919
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-03-14 11:25:26 -0400

    Update BrainlyUnlocker.user.js

--- BrainlyUnlocker.user.js
+++ BrainlyUnlocker.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         BrainlyUnlocker
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        *brainly.com/*
+// @match        *://unlock.wmeluna.com/*brainly.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=brainly.com
 // @grant        none
 // ==/UserScript==
