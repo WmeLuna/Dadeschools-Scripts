66df12f37bca2d0c84a9ffc8ca1821f57aabc542
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-03-17 13:58:57 -0400

    Update AutoUnblocker.user.js

--- AutoUnblocker.user.js
+++ AutoUnblocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoUnblocker
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       WmeLuna
 // @match        https://localhost:6543/block_screen.html?id=*
@@ -12,7 +12,7 @@
 (function() {
     'use strict';
     var theme = document.createElement('style');
-    theme.innerText=`body{background:#000}.blockScreen-svg g{stroke:purple!important}.blockScreen-stripe{background:purple!important}`
+    theme.innerText=`body{background:#000}.blockScreen-svg g{stroke:white!important}.blockScreen-stripe{background:purple!important}.blockScreen{flex-direction: row!important}.blockScreen-systemMessage,.blockScreen-customBodyText{display:none}.blockScreen-customSchoolName{position: absolute; z-index: 999; left: 70px;}.blockScreen-svg {position: absolute; z-index: 999; left: 10px;}`
     document.getElementsByTagName('head')[0].appendChild(theme);
 
     var scr = document.createElement('script');
