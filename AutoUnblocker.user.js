e0255a2a4bbc7167fa219b35dcf9dd78c883bb86
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-03-17 14:06:15 -0400

    Update AutoUnblocker.user.js

--- AutoUnblocker.user.js
+++ AutoUnblocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoUnblocker
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       WmeLuna
 // @match        https://localhost:6543/block_screen.html?id=*
@@ -12,7 +12,7 @@
 (function() {
     'use strict';
     var theme = document.createElement('style');
-    theme.innerText=`body{background:#000}.blockScreen-svg g{stroke:white!important}.blockScreen-stripe{background:purple!important}.blockScreen{flex-direction: row!important}.blockScreen-systemMessage,.blockScreen-customBodyText{display:none}.blockScreen-customSchoolName{position: absolute; z-index: 999; left: 70px;}.blockScreen-svg {position: absolute; z-index: 999; left: 10px;}`
+    theme.innerText=`body{background:#000}.blockScreen-svg g{stroke:white!important}.blockScreen-stripe{background:purple!important}.blockScreen{flex-direction: row!important}.blockScreen-customSchoolName{position: absolute; z-index: 999; left: 70px;}.blockScreen-svg {position: absolute; z-index: 999; left: 10px;}.blockScreen-systemMessage, .blockScreen-customBodyText {margin:unset; color:white; position: absolute; z-index: 999; right: 10px;}`
     document.getElementsByTagName('head')[0].appendChild(theme);
 
     var scr = document.createElement('script');
@@ -20,3 +20,4 @@
     scr.innerText=`function b(){0==a.href.includes("localhost")?location.href="https://unlock.wmeluna.com/main/"+a.href:setTimeout(b,250)}b();`
     document.getElementsByTagName('head')[0].appendChild(scr)
 })();
+
