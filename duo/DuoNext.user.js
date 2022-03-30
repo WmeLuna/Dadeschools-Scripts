e63c7c307b3f8bc76479350239204c12afeeeb4e
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-03-30 14:09:23 -0400

    Update DuoNext.user.js

--- duo/DuoNext.user.js
+++ duo/DuoNext.user.js
@@ -2,11 +2,11 @@
 // @name         DuoNext
 // @namespace    http://tampermonkey.net/
 // @version      0.1
+// @downloadURL  https://github.com/WmeLuna/Dadeschools-Scripts/raw/main/duo/DuoNext.user.js
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.duolingo.com/*
 // @icon         https://www.google.com/s2/favicons?domain=duolingo.com
-// @downloadURL  https://github.com/WmeLuna/Dadeschools-Scripts/raw/main/duo/DuoNext.user.js
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
@@ -18,10 +18,10 @@
         if(document.querySelector('[data-test="player-next"]').classList[0] != 'LhRk3') {
             document.querySelector('[data-test="player-next"]').click()
         }
-        setTimeout(next, 50);
+        setTimeout(next, 100);
         }
         catch {
-        setTimeout(next, 50);
+        setTimeout(next, 100);
         }
     }
     next()
