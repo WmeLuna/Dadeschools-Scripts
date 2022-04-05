85051826fb8e3480de406fe7f4fde6d748cdb37a
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-04-05 09:05:17 -0400

    Update BrainlyUnlocker.user.js

--- BrainlyUnlocker.user.js
+++ BrainlyUnlocker.user.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        *brainly.com/*
-// @match        *://unlock.wmeluna.com/*brainly.com/*
+// @match        *://unlock.wmeluna.com/*brainly.com*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=brainly.com
 // @grant        none
 // ==/UserScript==
