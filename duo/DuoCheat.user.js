d792ba0468d261be526fc430b6b7214c528da87b
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-04-20 14:35:54 -0400

    Update DuoCheat.user.js

--- duo/DuoCheat.user.js
+++ duo/DuoCheat.user.js
@@ -7,7 +7,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.duolingo.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=duolingo.com
+// @icon         https://github.com/WmeLuna/Dadeschools-Scripts/raw/main/duo/12215d986b2905ef9483.png
 // @grant        none
 // ==/UserScript==
 
