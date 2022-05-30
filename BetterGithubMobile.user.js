f4e3c13cae9c8435173050d2e84a102978e1b573
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-05-30 10:43:48 -0400

    Create BetterGithubMobile.user.js

---
+++ BetterGithubMobile.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         BetterGithubMobile
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/*
+// @icon         https://github.com/fluidicon.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var topSidebar = document.createElement("style")
+    topSidebar.innerText = '#js-repo-pjax-container .Layout{display:flex!important;flex-direction:column!important}.Layout .Layout-main{order:2}.Layout .Layout-sidebar{order:1}'
+    document.head.appendChild(topSidebar)
+    // Your code here...
+})();
