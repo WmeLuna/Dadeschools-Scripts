2c44df1a0f8d64074c464c6c7ab6fc221f2204e1
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-10-18 09:20:30 -0400

    bye bye cumcord

--- themes/AutoDiscordTheme.user.js
+++ themes/AutoDiscordTheme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoDiscordTheme
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       WmeLuna
 // @match        https://unlock.wmeluna.com/main/*discord.com*
@@ -10,10 +10,9 @@
 // @run-at       document-end
 // ==/UserScript==
 
-(async function() {
+(function() {
     'use strict';
     var theme = document.createElement('style');
     theme.innerText=`@import url('https://lucord.wmeluna.com/theme.css')`
     document.getElementsByTagName('head')[0].appendChild(theme);
-    eval(await (await fetch("https://raw.githubusercontent.com/Cumcord/builds/main/build.js", {cache: "no-store"})).text());
 })();
